bae6767a23fcf3a9203cf9707109702b242a760f
Leirthedragon <94846764+Leirthedragon@users.noreply.github.com>
2021-11-22 22:41:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,18 @@ params = {
 while not quit:
     SchoolApi("schoolInfo", params).get_school_info()
 
-    dobby_say("무엇을 하시겠습니다?")
+    dobby_say("무엇을 하고싶으신가요?")
     msg = input("me: ")
 
     if "급식" in msg:
-        dobby_say("급식을 선택하셨습니다")
+        dobby_say("급식? 급식 말입니까? 알겠습니다!")
         params = {
             "MLSV_YMD":  str(input("어느날의 급식을 알고 싶으세요? (YYYYMMDD) : ")),
         }
         dobby_say(SchoolApi("mealServiceDietInfo", params).meal())
 
     elif "시간표" in msg:
-        dobby_say("시간표을 선택하셨습니다")
+        dobby_say("시간표를 준비하겠습니다, 주인님!")
         params = {
             "ALL_TI_YMD":  str(input("시간표일자(YYYYMMDD) : ")),
             "GRADE":  str(input("학년 : ")),
@@ -33,18 +33,18 @@ while not quit:
         dobby_say(SchoolApi("hisTimetable", params).get_data())
 
     elif "학사일정" in msg:
-        dobby_say("학사일정을 선택하셨습니다")
+        dobby_say("주인님의 이번 학사일정은....")
         params = {
             "AA_YMD":  str(input("학사일자(YYYYMMDD) : "))
         }
         dobby_say(SchoolApi("SchoolSchedule", params).schedule())
 
     elif "도와줘" in msg or "도움말" in msg or "help" in msg:
-        dobby_say("도움말을 선택하셨습니다\n"+
-        "급식을 알고싶으시면 급식을 입력하세요\n"+
-        "시간표를 알고싶으시면 시간표를 입력하세요\n"+
-        "학사일정을 알고싶으시면 학사일정을 입력하세요\n"+
-        "종료하려면 양말을 입력하세요")
+        dobby_say("도비의 도움말입니다..!\n"+
+        "급식을 알고싶으시다면 급식을 입력해주세요\n"+
+        "시간표를 알고싶으다면 시간표를 입력해주세요\n"+
+        "학사일정을 알고싶으다면 학사일정을 입력해주세요!\n"+
+        "저를 종료시키시려면 양말을 입력해주세요!")
     
     elif "양말" in msg or "exit" in msg or "quit" in msg:
         dobby_say("양말을 도비에게 주었어요\n도비는 자유에요")
