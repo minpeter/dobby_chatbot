c00b3f3fadc864e77d8433441d6ba034a254ab5c
minpeter <kali2005611@gmail.com>
2021-12-05 23:28:25 +0900

    old code

--- filemanager.py
+++ filemanager.py
@@ -13,44 +13,14 @@ def readfile(filename):
     except:
         return 'file read error ❌'
 
-def prtBanner():
-    print(readfile(file_dir("banner.txt")))
-
 def readjson(filename):
     return json.loads(readfile(filename))
-
-def read_school_info(key):
-    try:
-        data = readjson(file_dir("school_info.json"))
-    except:
-        print("school_info 파일이 json 형식인지 확인해주세요")
-    try:
-        if data[key] != "" or data[key] != None:
-            return {key:data[key]}
-        else:
-            return {key:None}
-    except:
-        return {key:None}
-    
 
 def read_api_key(key):
     return readjson(file_dir("api_key.json"))[key]
 
-def writefile(filename, data):
-    try:
-        with open(filename,'r+') as f:
-            f.read()
-            f.seek(0)
-            f.write(data)
-            f.truncate()
-    except:
-        return '오류발생'
-    
-def writejson(filename, data):
-    writefile(filename, json.dumps(data))
-    
-def write_school_info(data):
-    writejson(file_dir("school_info.json"), data)
+def prtBanner():
+    print(readfile(file_dir("banner.txt")))
 
 def read_quiz():
     return readjson(file_dir("quiz.json"))
---
+++ old_code/old_Marble.py
@@ -0,0 +1,60 @@
+from interface import *
+import random
+
+
+class Marble:
+    def __init__(self):
+        self.result = 0 # 매턴 마다 홀짝 결과 저장
+        self.expec = 0 # 매턴 홀짝 예상 값
+        self.dobby = 4 # 도비의 잔여 구슬 갯수
+        self.player = 4 # player의 잔여 구슬 갯수
+
+    def game(self):
+        quit = False
+        while not quit:
+            if self.dobby == 0:
+                quit = True
+                dobby_say(f"주인님이 승리했어요!")
+
+            elif self.player == 0:
+                quit = True
+                dobby_say(f"도비가 승리했어요!")
+
+            else:
+                self.turn()
+                self.oddeven()
+                self.count()
+                self.status()
+
+    def turn(self):
+        dobby_say("홀짝을 예상해주세요!\n"+
+                  "  1) 홀수\n"+
+                  "  2) 짝수")
+        self.expec = int(my_answer())    #예상값을 입력
+
+    def oddeven(self):
+        number_list = random.choice(range(1, self.dobby+1))
+        if number_list % 2 == 0:
+            dobby_say(f"{number_list}이므로 짝수입니다!")
+            self.result = 2
+        else:
+            dobby_say(f"{number_list}이므로 홀수입니다!")
+            self.result = 1
+
+    def count(self):
+        if self.result == self.expec:    #예상에 성공한 경우 도비의 구슬이 감소
+            self.dobby -= 1
+            dobby_say("주인님이 맞추셨어요!!\n"+
+                      "도비의 구슬을 가져가셔도 좋아요....")
+        
+        elif self.result != self.expec:    #실패한 경우 플레이어의 구슬 감소
+            self.player -= 1
+            dobby_say("아이쿠..주인님 틀리셨네요\n"+
+                      "주인님의 구슬은 이제 제 것입니다!")
+
+    def status(self):
+        dobby_say(f"도비의 구슬은 {self.dobby}개 입니다.\n"+
+                  f"플레이어의 구슬은 {self.player}개 입니다.")
+
+if __name__ == "__main__":
+    Marble().game()
